3659fd151ba331cc4cd55ea7fdac6057a69dd228
geofannyher <geofannyher@gmail.co>
2024-07-11 15:57:59 +0700

    update

--- src/app/api/audio/route.ts
+++ src/app/api/audio/route.ts
@@ -5,14 +5,14 @@ export async function POST(req: Request) {
   const { text } = await req.json();
   try {
     const result = await axios.post(
-      "https://api.elevenlabs.io/v1/text-to-speech/xM0TBYBF0vSckov9kDQy",
+      "https://api.elevenlabs.io/v1/text-to-speech/9PT7w7JPpD5X2qcOmOEb",
       {
         text: text,
         model_id: "eleven_multilingual_v2",
         voice_settings: {
-          stability: 0.4,
-          similarity_boost: 0.95,
-          style: 0.15,
+          stability: 0.5,
+          similarity_boost: 1,
+          style: 0,
           use_speaker_boost: true,
         },
       },
