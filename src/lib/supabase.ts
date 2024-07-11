1b14af14969e8916d200531fedc6ae96312ab5c1
geofannyher <geofannyher@gmail.co>
2024-07-11 14:56:53 +0700

    update

--- src/lib/supabase.ts
+++
@@ -1,5 +0,0 @@
-import { createClient } from "@supabase/supabase-js";
-
-const supabaseUrl: any = process.env.NEXT_SUP_URL;
-const supabaseKey: any = process.env.NEXT_SUP_KEY;
-export const supabase = createClient(supabaseUrl, supabaseKey);
