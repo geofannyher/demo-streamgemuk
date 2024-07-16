e83688024b5dce9c8df9defd0af7df0a3c0465be
geofannyher <geofannyher@gmail.co>
2024-07-16 11:26:52 +0700

    update data audio

--- src/app/shared/audio.ts
+++ src/app/shared/audio.ts
@@ -1,7 +1,7 @@
 export const audios = [
   {
     name: "Anker TWS Earbuds Soundcore R50i - A3949 Bluetooth Earbud",
-    url: "https://res.cloudinary.com/dp8ita8x5/video/upload/v1720681881/videoStream/gemuk/zgt44fb5py31dklztitz.mp3",
+    url: "https://res.cloudinary.com/dp8ita8x5/video/upload/v1721103230/videoStream/gemuk/new/Anker_TWS_Earbuds_Soundcore_R50i_A3949_Bluetooth_Earbud_2.mp3",
   },
   {
     name: "Cek Keranjang Kuningnya",
@@ -12,8 +12,8 @@ export const audios = [
     url: "https://res.cloudinary.com/dp8ita8x5/video/upload/v1720681993/videoStream/gemuk/kuknewbryty21xseosoc.mp3",
   },
   {
-    name: "F999+headphone  sound card",
-    url: "https://res.cloudinary.com/dp8ita8x5/video/upload/v1720682030/videoStream/gemuk/didoip3f4hlbkplm57tx.mp3",
+    name: "F999+headphone sound card",
+    url: "https://res.cloudinary.com/dp8ita8x5/video/upload/v1721103448/videoStream/gemuk/new/F999%20headphone%20%20sound%20card.mp3",
   },
   {
     name: "Garansi 12 Bulan",
@@ -29,39 +29,43 @@ export const audios = [
   },
   {
     name: "Hyundai T26 Wireless TWS Earphone Bluetooth",
-    url: "https://res.cloudinary.com/dp8ita8x5/video/upload/v1720682273/videoStream/gemuk/xkhlifqwuqupqhp1lwgu.mp3",
+    url: "https://res.cloudinary.com/dp8ita8x5/video/upload/v1721103184/videoStream/gemuk/new/Hyundai%20T26%20Wireless%20TWS%20Earphone%20Bluetooth.mp3",
   },
   {
     name: "Lenovo Thinkplus XT80 True Wireless Bluetooth Earphone",
-    url: "https://res.cloudinary.com/dp8ita8x5/video/upload/v1720682301/videoStream/gemuk/dka5x2cm8vlzmiiquecv.mp3",
+    url: "https://res.cloudinary.com/dp8ita8x5/video/upload/v1721103206/videoStream/gemuk/new/Lenovo%20Thinkplus%20XT80%20True%20Wireless%20Bluetooth%20Earphone.mp3",
   },
   {
     name: "LENYES S220 12W Mini Transparan Wireless Speaker",
-    url: "https://res.cloudinary.com/dp8ita8x5/video/upload/v1720682343/videoStream/gemuk/uzrhh3mkdmgdlk2pezjg.mp3",
+    url: "https://res.cloudinary.com/dp8ita8x5/video/upload/v1721103332/videoStream/gemuk/new/LENYES%20S220%2012W%20Mini%20Transparan%20Wireless%20Speaker.mp3",
   },
   {
-    name: "MIC-206  MIC-101 PRO Advance UHF",
-    url: "https://res.cloudinary.com/dp8ita8x5/video/upload/v1720682371/videoStream/gemuk/qituynvbyava1dpau2jb.mp3",
+    name: "MIC-206 MIC-101 PRO Advance UHF",
+    url: "https://res.cloudinary.com/dp8ita8x5/video/upload/v1721103430/videoStream/gemuk/new/MIC-206%20%20MIC-101%20PRO%20Advance%20UHF.mp3",
+  },
+  {
+    name: "【GOOJODOQ】 Speaker Karaoke Bluetooth",
+    url: "https://res.cloudinary.com/dp8ita8x5/video/upload/v1721103379/videoStream/gemuk/new/%E3%80%90GOOJODOQ%E3%80%91%20Speaker%20Karaoke%20Bluetooth.mp3",
   },
   {
     name: "Monster XKT25 Wireless Bluetooth",
-    url: "https://res.cloudinary.com/dp8ita8x5/video/upload/v1720682395/videoStream/gemuk/vfdiocfgtzwyulvk6wmb.mp3",
+    url: "https://res.cloudinary.com/dp8ita8x5/video/upload/v1721103401/videoStream/gemuk/new/Monster%20XKT25%20Wireless%20Bluetooth.mp3",
   },
   {
     name: "Monster_AC100MH22156_Wireless_Headset_Bluetooth_5_3_OWS_Waterproof",
-    url: "https://res.cloudinary.com/dp8ita8x5/video/upload/v1720682423/videoStream/gemuk/mxhnavk8f1qn8mjgslpn.mp3",
+    url: "https://res.cloudinary.com/dp8ita8x5/video/upload/v1721103150/videoStream/gemuk/new/Monster_AC100MH22156_Wireless_Headset_Bluetooth_5_3_OWS_Waterproof.mp3",
   },
   {
     name: "Robot TWS Wireless Earphone Airbuds New T50S",
-    url: "https://res.cloudinary.com/dp8ita8x5/video/upload/v1720682455/videoStream/gemuk/v9tbqsrhsgj13pza63mv.mp3",
+    url: "https://res.cloudinary.com/dp8ita8x5/video/upload/v1721103256/videoStream/gemuk/new/Robot%20TWS%20Wireless%20Earphone%20Airbuds%20New%20T50S.mp3",
   },
   {
     name: "Soundcore Anker R50i NC Earbuds",
-    url: "https://res.cloudinary.com/dp8ita8x5/video/upload/v1720682487/videoStream/gemuk/fcnujhzfkclruilfmfqp.mp3",
+    url: "https://res.cloudinary.com/dp8ita8x5/video/upload/v1721103117/videoStream/gemuk/new/Soundcore%20Anker%20R50i%20NC%20Earbuds.mp3",
   },
   {
     name: "V8S sound card singing recording professional",
-    url: "https://res.cloudinary.com/dp8ita8x5/video/upload/v1720682516/videoStream/gemuk/fljl9rk9ebm3xn8k3pig.mp3",
+    url: "https://res.cloudinary.com/dp8ita8x5/video/upload/v1721103358/videoStream/gemuk/new/V8S%20sound%20card%20singing%20recording%20professional.mp3",
   },
   {
     name: "Yang Baru  Datang Cek Etalase",
