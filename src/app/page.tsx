f3094f3c25c6dbd1a3c6d06b22914fc30337517c
geofannyher <geofannyher@gmail.com>
2024-08-09 10:49:03 +0700

    update video no sound

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,7 +4,7 @@ import React, { useEffect, useRef, useState } from "react";
 
 const PlayVideo: React.FC = () => {
   const videoUrl =
-    "https://res.cloudinary.com/dp8ita8x5/video/upload/v1720685155/videoStream/gemuk/wpoydfqeewnhdvtr9mog.mp4";
+    "https://res.cloudinary.com/dp8ita8x5/video/upload/v1723175258/videoStream/gemuk/fix%20no%20sound.mp4";
 
   const [audioUrl, setAudioUrl] = useState<string>("");
   const videoRef = useRef<HTMLVideoElement>(null);
